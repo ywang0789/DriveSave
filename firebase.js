fe8eb8b4d828b84bea6192a014fb99a6c2289713
ywang0789 <ywang0789@conestogac.on.ca>
2024-01-03 14:01:38 -0500

    hide + change api keys
    - regenerated new api keys
    - Updated firebase.js to use environment variables for Firebase configuration
    - Modified .gitignore to exclude my_keys files

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCwNAaeMxAWGsg2AeOFz9U8QDGSfS3OuVY",
-    authDomain: "drivesave-69420.firebaseapp.com",
-    projectId: "drivesave-69420",
-    storageBucket: "drivesave-69420.appspot.com",
-    messagingSenderId: "1019387797693",
-    appId: "1:1019387797693:web:dd3358a3c87bf59259d7d4",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
