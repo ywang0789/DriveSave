54069217ac611da995350adc482ec609dbfb3a7e
ywang0789 <ywang0789@conestogac.on.ca>
2024-01-04 11:57:52 -0500

    added back buttons to all screens
    - TODO: organize styles and components for reuse

---
+++ components/AppComponents.js
@@ -0,0 +1,18 @@
+// TODO: Create reusable components here
+// TODO: add components to all screens
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import {AppStyles} from '../styles/AppStyles';
+
+
+
+
+// example code
+// const ExampleButton = ({ onPress, title }) => (
+//   <TouchableOpacity style={AppStyles.button} onPress={onPress}>
+//     <Text style={AppStyles.buttonText}>{title}</Text>
+//   </TouchableOpacity>
+// );
+
+
+// export default ExampleButton;
